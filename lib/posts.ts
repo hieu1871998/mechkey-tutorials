4ee426d8a6f9ad6ed2b4f2857f4db3713e595c42
Vi Minh Hiếu <hieu1871998@gmail.com>
2022-03-13 12:13:22 +0700

    remove md extension in getPostData

--- lib/posts.ts
+++ lib/posts.ts
@@ -173,7 +173,7 @@ export const getAllPostIds = () => {
 }
 
 export const getPostData = async (category: string, id: string) => {
-  const fullPath = path.join(postsDirectory, `${category}`, `${id}.md`);
+  const fullPath = path.join(postsDirectory, `${category}`, `${id}`);
   const fileContents = fs.readFileSync(fullPath, 'utf-8');
 
   const matterResult = matter(fileContents);
