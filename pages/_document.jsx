a3a2f879e65777be1f97c0e03b5f2e607d916d89
Vi Minh Hiếu <hieu1871998@gmail.com>
2022-03-15 17:07:36 +0700

    changed to jsx because of annoying type errors

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/display-name */
 import * as React from 'react';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import { Stylesheet, resetIds } from '@fluentui/react';
